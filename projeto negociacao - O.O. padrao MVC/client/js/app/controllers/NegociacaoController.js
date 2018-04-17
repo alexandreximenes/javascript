247eda43df2bd1f02ba1a1b617feea346e631e81
alexandre ximenes <xyymenes@gmail.com>
2018-04-16 22:24:34 -0300

    criando a classe ListaNegociacoes com programacao defensiva

--- projeto negociacao - O.O. padrao MVC/client/js/app/controllers/NegociacaoController.js
+++ projeto negociacao - O.O. padrao MVC/client/js/app/controllers/NegociacaoController.js
@@ -10,34 +10,53 @@ class NegociacaoController{
 		this._inputData = $('#data');
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');	
-		
+		this._listaNegociacoes = new ListaNegociacoes();
 		//let form = $(".form");
-
 	}
 
 	adiciona(event){
 		event.preventDefault();
 
-		//console.log(typeof(this._inputData.value));
-
-		//debugger;
-
-		//Spread operator
-		/*let data = new Date(...this._inputData.value
-			.split('-')
-			.map((item, indice) => item - indice % 2)
-		);*/
-
-		;
-
 		// criando um objeto Negociacao
-		let negociacao = new Negociacao(
+		this._listaNegociacoes.adiciona(this._criaNegociacao());
+		this._listaNegociacoes.imprime;
+		this._limpaFormulario();
+			
+
+	}
+	_criaNegociacao(){
+		return new Negociacao(
 				DateHelper.textoParaData(this._inputData.value),
 				this._inputQuantidade.value,
 				this._inputValor.value
 			);
-			console.warn(this._inputData.value);
-			console.log("DateHelper.dataParaTexto = "+DateHelper.dataParaTexto(new Date(this._inputData.value) ));
+	}
+
+	_limpaFormulario(){
+		let form = $('.form');
+		form.reset();
+		form.data.focus();
+	}
+
+}
+//tempo em ms do carregamento deste arquivo (performance)
+console.timeEnd("Classe NegociacaoController");
+
+
+//console.log(typeof(this._inputData.value));
+
+//debugger;
+
+//Spread operator
+/*let data = new Date(...this._inputData.value
+	.split('-')
+	.map((item, indice) => item - indice % 2)
+);*/
+
+
+/*this._listaNegociacoes.negociacoes.forEach(negociacao => {
+				console.log("negociacao array : "+negociacao);
+			});*/
 
 			/*console.group();
 			//console.log("Data e Time  : "+new Date(new Date().getTime()));
@@ -47,8 +66,5 @@ class NegociacaoController{
 			console.groupEnd();
 			console.table(negociacao);*/
 
-	}
 
-}
-//tempo em ms do carregamento deste arquivo (performance)
-console.timeEnd("Classe NegociacaoController");
+
