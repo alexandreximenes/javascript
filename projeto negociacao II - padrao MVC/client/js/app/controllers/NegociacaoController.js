1ae57b894575ca814f46c7fa53647e0b719d522f
Alexandre Ximenes <xyymenes@gmail.com>
2018-04-22 19:19:23 -0300

    Refatorando codigo

--- projeto negociacao II - padrao MVC/client/js/app/controllers/NegociacaoController.js
+++ projeto negociacao II - padrao MVC/client/js/app/controllers/NegociacaoController.js
@@ -12,19 +12,15 @@ class NegociacaoController{
 		this._inputValor = $('#valor');	 
 		
 		let self = this;
-		this._listaNegociacoes = ProxyFactory.create(
-			new ListaNegociacoes(), 
-			['adiciona', 'esvazia'],  
-			(model) => this._negociacoesView.update(model) );
-
-		this._negociacoesView = new NegociacoesView($('.negociacoesView'));
-		this._mensagem = ProxyFactory.create( 
-			new Mensagem(), ['texto'], 
-			(model) => this._mensagemView.update(this._mensagem) );	
 		
+		this._listaNegociacoes = new Bind(new ListaNegociacoes(), new NegociacoesView($(".negociacoesView")), "adiciona", "esvazia");
+
 		this._mensagemView = new MensagemView($('.mensagemView'));
+		
+		this._mensagem = new Bind(new Mensagem(), this._mensagemView, "texto");
+		
 		this._mensagemView.update(this._mensagem);
-		//this._negociacoesView.update(this._listaNegociacoes);
+
 	}
 
 	adiciona(event){
