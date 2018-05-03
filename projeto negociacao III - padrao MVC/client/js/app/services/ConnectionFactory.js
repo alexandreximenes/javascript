85d84bf8906ac48380b1da152895f6132de7a6f9
Alexandre Ximenes <xyymenes@gmail.com>
2018-05-03 11:25:24 -0300

    Usando Padrao de projeto Module Pattern

--- projeto negociacao III - padrao MVC/client/js/app/services/ConnectionFactory.js
+++ projeto negociacao III - padrao MVC/client/js/app/services/ConnectionFactory.js
@@ -1,50 +1,41 @@
+var ConnectionFactory = (function(){
+    
+        var stores = ["negociacoes"];
+        var dbName = "negociacoesDB";
+        var version = 4;
+        var connection = null;
 
-var stores = ['negociacoes'];
-var dbName = 'negociacoesDB';
-var version = 4;
-var connection = null;
-
-return class ConnectionFactory{
-
-    constructor(){
-        throw new Error('Não é possivel criar instancias de ConnectionFactory');
-    }
-    static getConnection(){
-        
-        return new Promise( (resolve, reject) => {
-
+        return class ConnectionFactory {
+        constructor() {
+            throw new Error("Não é possivel criar instancias de ConnectionFactory");
+        }
+        static getConnection() {
+            return new Promise((resolve, reject) => {
             let openRequest = window.indexedDB.open(dbName, version);
 
             openRequest.onupgradeneeded = e => {
-
-                resolve( ConnectionFactory._createStores(e.target.result) );
-                
+                resolve(ConnectionFactory._createStores(e.target.result));
             };
             openRequest.onsuccess = e => {
-                
-                if(!connection) connection = e.target.result;
+                if (!connection) connection = e.target.result;
                 console.log(connection);
-                resolve( connection );
+                resolve(connection);
             };
 
             openRequest.onerror = e => {
-
                 console.log(e.target.error.name);
-                reject( e.target.error.name );
+                reject(e.target.error.name);
             };
-            
-        });
-    }
-    
-    static _createStores(connection){
-        
-        stores.forEach( store => {
-            if (connection.objectStoreNames.contains(store)) 
-                connection.deleteObjectStore(store); 
-            
-            connection.createObjectStore(store, { autoIncrement: true });
-    
-        });
+            });
+        }
 
-    }
-}
+        static _createStores(connection) {
+            stores.forEach(store => {
+            if (connection.objectStoreNames.contains(store))
+                connection.deleteObjectStore(store);
+
+            connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
+        }
+})();
