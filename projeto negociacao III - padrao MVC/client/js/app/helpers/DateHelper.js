533580a7084082a69a4f3ed759b21aadee59ddf8
Alexandre Ximenes <xyymenes@gmail.com>
2018-05-04 10:02:02 -0300

    usando metodos da api javascript

--- projeto negociacao III - padrao MVC/client/js/app/helpers/DateHelper.js
+++ projeto negociacao III - padrao MVC/client/js/app/helpers/DateHelper.js
@@ -7,14 +7,13 @@ class DateHelper {
     
     static dataParaTexto(data) {
         
-        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+        return data.toLocaleDateString();//`${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
     
     static textoParaData(texto) {
-        
-        if(!/\d{2}\/\d{2}\/\d{4}/.test(texto)) 
-            throw new Error('Deve estar no formato dd/mm/aaaa');
-             
-        return new Date(...texto.split('/').reverse().map((item, indice) => item - indice % 2));
+        console.log(texto);
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error("Data deve estar no formato aaaa-mm-dd");
+		
+		return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 }
