736bc2d584e8c863deff685cb189f5e0ca93ece3
Alexandre Ximenes <xyymenes@gmail.com>
2018-09-19 23:14:39 -0300

    continuando curso

--- Introducao ao JavaScript/js/form.js
+++ Introducao ao JavaScript/js/form.js
@@ -1,13 +1,24 @@
 let form = document.querySelector("#form-add");
+let ul = document.querySelector(".ul");
 
+function validaPaciente(pessoa){
+    let erros = [];
+    pessoa.nome == "" || pessoa.length <= 2 ? erros.push('Nome invalido!') : '';
+    pessoa.peso == "" || (pessoa.peso <= 0 || pessoa.peso > 1000) ? erros.push('Peso invalido!') :'';
+    pessoa.altura == ""  || (pessoa.altura <=0 || pessoa.altura >= 4.0)? erros.push('Altura invalida!'):'';
+    pessoa.gordura == ""  || pessoa.gordura < 0 || pessoa.gordura > pessoa.peso ? erros.push('Gordura invalida!') : '';
+    console.log(pessoa);
+    return erros;
+}
 function getDadosPessoa(form){
 
     let pessoa = {
         nome : form.nome.value,
-        altura : form.altura.value,
         peso : form.peso.value,
-        gordura : form.gordura.value
-    }
+        altura : form.altura.value,
+        gordura : form.gordura.value,
+        imc : (this.peso / (this.altura * this.altura)).toFixed(2)
+    };
     return pessoa;
 }
 
@@ -19,14 +30,35 @@ document.querySelector('#adicionar-paciente')
          * @type {{nome: *, altura: *, peso: *, gordura: *}}
          */
         let pessoa = getDadosPessoa(form);
-
-        //using template string
-        console.log(`${pessoa.nome} tem ${pessoa.altura}mt de altura e ${pessoa.peso} em Kg, com gordura de ${pessoa.gordura}`);
-
+        let erros = validaPaciente(pessoa);
+        if(erros.length > 0) {
+            console.log(erros);
+             cria_li(erros);
+             return;
+        }else{
         criaTr(pessoa);
+            ul.innerHTML = '';
+            ul.classList.remove('erros');
+
+            //using template string
+            console.log(`${pessoa.nome} tem ${pessoa.altura}mt de altura e ${pessoa.peso}Kg, com gordura de ${pessoa.gordura}, totalizando IMC de : ${pessoa.imc}`);
+        }
+
+        form.reset();
+        form.nome.focus();
+
 
     });
 
+function cria_li(erros){
+    ul.innerHTML = '';
+    ul.classList.add('erros');
+    erros.forEach(erro => {
+        let li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 function criaTr(pessoa){
     /**
      * Cria um TR
@@ -54,7 +86,7 @@ function criaTr(pessoa){
     pesoTd.textContent = pessoa.peso;
     alturaTd.textContent = pessoa.altura;
     gorduraTd.textContent = pessoa.gordura;
-    imcTd.textContent = (pessoa.peso / (pessoa.altura * pessoa.altura)).toFixed(2);
+    imcTd.textContent = pessoa.imc;
 
     /**
      * Coloca as TDs criadas dentro da TR criada
