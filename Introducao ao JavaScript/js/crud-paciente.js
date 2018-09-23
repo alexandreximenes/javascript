8d0d20a61eaf13dddb8088d4c02306b7286acb90
Alexandre Ximenes <xyymenes@gmail.com>
2018-09-23 02:01:42 -0300

    criando funcao de copiar pessoa, excluir, criar uma pessoa com base na antiga

--- Introducao ao JavaScript/js/crud-paciente.js
+++ Introducao ao JavaScript/js/crud-paciente.js
@@ -1,15 +1,58 @@
 var tabela = document.querySelector("#tabela-pacientes");
 
+/**
+ * Remove uma pessoa da tabela
+ */
 tabela.addEventListener('dblclick', (e) => {
-   let TR = e.target.parentNode;
-   TR.classList.add('fadeout');
-   setTimeout(() => {
-       TR.remove();
-   }, 500);
+    let TR = e.target.parentNode;
+    TR.classList.add('fadeout');
+    setTimeout(() => {
+        TR.remove();
+    }, 500);
 });
 
+/**
+ * Copia a linha (pessoa) clicada para a tabela
+ */
+tabela.addEventListener('click', (e) => {
+    let tdNome = e.path[0].className;
+    if(tdNome === 'info-nome'){
+        let TR = e.target.parentNode;
 
+        let form = document.querySelector("#form-add");
 
+        var pessoa = capturaPessoa(TR);
+
+        gerarPessoa(pessoa.nome, pessoa.peso, pessoa.altura, pessoa.gordura);
+
+    }
+
+    form.reset();
+    form.nome.focus();
+
+});
+
+tabela.addEventListener('mouseover', (e) => {
+    let TR = e.target.parentNode;
+    let form = document.querySelector("#form-add");
+
+    var pessoa = capturaPessoa(TR);
+
+    form.nome.value = pessoa.nome;
+    form.peso.value = pessoa.peso;
+    form.altura.value = pessoa.altura;
+    form.gordura.value = pessoa.gordura;
+
+});
+
+function capturaPessoa(TR) {
+    let nome = TR.querySelector('.info-nome').textContent;
+    let peso = TR.querySelector('.info-peso').textContent;
+    let altura = TR.querySelector('.info-altura').textContent;
+    let gordura = TR.querySelector('.info-gordura').textContent;
+
+    return getDadosPessoa(nome, peso, altura, gordura);
+}
 
 /*
 var pacientes = document.querySelectorAll(".paciente");
--- Introducao ao JavaScript/js/form.js
+++ Introducao ao JavaScript/js/form.js
@@ -7,17 +7,17 @@ function validaPaciente(pessoa){
     pessoa.peso == "" || (pessoa.peso <= 0 || pessoa.peso > 1000) ? erros.push('Peso invalido!') :'';
     pessoa.altura == ""  || (pessoa.altura <=0 || pessoa.altura >= 4.0)? erros.push('Altura invalida!'):'';
     pessoa.gordura == ""  || pessoa.gordura < 0 || pessoa.gordura > pessoa.peso ? erros.push('Gordura invalida!') : '';
-    console.log(pessoa);
+    // console.log(pessoa);
     return erros;
 }
-function getDadosPessoa(form){
+function getDadosPessoa(nome, peso, altura, gordura){
 
     let pessoa = {
-        nome : form.nome.value,
-        peso : form.peso.value,
-        altura : form.altura.value,
-        gordura : form.gordura.value,
-        imc : (this.peso / (this.altura * this.altura)).toFixed(2)
+        nome : nome,
+        peso : peso,
+        altura : altura,
+        gordura : gordura,
+        imc : (peso / (altura * altura)).toFixed(2) //this.peso / (this.altura * this.altura)).toFixed(2)
     };
     return pessoa;
 }
@@ -29,26 +29,37 @@ document.querySelector('#adicionar-paciente')
          *
          * @type {{nome: *, altura: *, peso: *, gordura: *}}
          */
-        let pessoa = getDadosPessoa(form);
+
+        var gerou = gerarPessoa(form.nome.value, form.peso.value, form.altura.value, form.gordura.value);
+        if(gerou){
+            form.reset();
+            form.nome.focus();
+        }
+    });
+
+function gerarPessoa(nome, peso, altura, gordura){
+    let pessoa = getDadosPessoa(nome, peso, altura, gordura);
+    if(pessoa != null){
         let erros = validaPaciente(pessoa);
         if(erros.length > 0) {
             console.log(erros);
-             cria_li(erros);
-             return;
+            cria_li(erros);
+            return false;
         }else{
-        criaTr(pessoa);
+            criaTr(pessoa);
             ul.innerHTML = '';
             ul.classList.remove('erros');
 
             //using template string
             console.log(`${pessoa.nome} tem ${pessoa.altura}mt de altura e ${pessoa.peso}Kg, com gordura de ${pessoa.gordura}, totalizando IMC de : ${pessoa.imc}`);
+            return true;
         }
+    }
 
-        form.reset();
-        form.nome.focus();
-    });
+}
 
 function cria_li(erros){
+    ul.classList.remove('fadeout');
     ul.innerHTML = '';
     ul.classList.add('erros');
     erros.forEach(erro => {
@@ -56,6 +67,9 @@ function cria_li(erros){
         li.textContent = erro;
         ul.appendChild(li);
     });
+    setTimeout(() => {
+        ul.classList.add('fadeout');
+    }, 4000);
 }
 function criaTr(pessoa){
     /**
@@ -63,28 +77,27 @@ function criaTr(pessoa){
      * @type {HTMLTableRowElement}
      */
     let pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
 
     /**
      * Cria varias colunas TD para colocar dentro da TR
      * @see pacienteTr
      * @type {HTMLTableDataCellElement}
      */
-    let nomeTd = document.createElement("td");
-    let pesoTd = document.createElement("td");
-    let alturaTd = document.createElement("td");
-    let gorduraTd = document.createElement("td");
-    let imcTd = document.createElement("td");
 
-    /**
-     * Atribuir dados da pessoa para as TDs
-     * @see @param pessoa
-     * @type {*}
-     */
-    nomeTd.textContent = pessoa.nome;
-    pesoTd.textContent = pessoa.peso;
-    alturaTd.textContent = pessoa.altura;
-    gorduraTd.textContent = pessoa.gordura;
-    imcTd.textContent = pessoa.imc;
+    function montarTD(clazz, conteudo) {
+        let td = document.createElement("td");
+        td.classList.add(clazz);
+        td.textContent = conteudo;
+        return td;
+    }
+
+    let nomeTd = montarTD('info-nome', pessoa.nome);//document.createElement("td");
+    let pesoTd = montarTD('info-peso', pessoa.peso);//document.createElement("td");
+    let alturaTd = montarTD('info-altura', pessoa.altura);//document.createElement("td");
+    let gorduraTd = montarTD('info-gordura', pessoa.gordura);//document.createElement("td");
+    let imcTd = montarTD('info-imc', pessoa.imc);//document.createElement("td");
+
 
     /**
      * Coloca as TDs criadas dentro da TR criada
