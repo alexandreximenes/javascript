b8b38978ee16c4f34eea1d3a9adbe12f9ff6cbad
Alexandre Ximenes <xyymenes@gmail.com>
2018-09-28 20:00:23 -0300

    Fazendo requisicao HTTP com Fetch

--- Introducao ao JavaScript/js/buscar-dados-server2.js
+++ Introducao ao JavaScript/js/buscar-dados-server2.js
@@ -1,30 +1,38 @@
 let buscar = document.querySelector('#buscar-paciente');
 buscar.addEventListener('click', () => {
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientes');
-
-    xhr.addEventListener('load', () => {
-        if(xhr.status == 200){
-            console.log(typeof xhr.responseText);
-            console.log(xhr.responseText);
-
-            let pacientes = JSON.parse(xhr.responseText);
-
-            pacientes.forEach((paciente) => {
-                adicionarPacienteNaTabela(paciente);
-            });
-            // console.log("JSON.parse(xhr.responseText); "+pacientes);
-            // console.log(typeof pacientes);
-
-            // pacientes = JSON.stringify(pacientes);
-            // console.log("pacientes = JSON.stringify(pacientes); "+pacientes);
-            // console.log(typeof pacientes);
-        }else{
-            console.log(xhr.status);
-            console.log(xhr.responseText);
-        }
-    });
-
-    xhr.send();
+    fetch('https://api-pacientes.herokuapp.com/pacientes')
+        //return one Promise
+        .then(r => r.json())
+        //return object
+        .then(pacientes => pacientes.forEach(paciente => adicionarPacienteNaTabela(paciente)))
+        .catch(log => console.log(log));
 });
+
+
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientes');
+//
+// xhr.addEventListener('load', () => {
+//     if(xhr.status == 200){
+//         console.log(typeof xhr.responseText);
+//         console.log(xhr.responseText);
+//
+//         let pacientes = JSON.parse(xhr.responseText);
+//
+//         pacientes.forEach((paciente) => {
+//             adicionarPacienteNaTabela(paciente);
+//         });
+//         // console.log("JSON.parse(xhr.responseText); "+pacientes);
+//         // console.log(typeof pacientes);
+//
+//         // pacientes = JSON.stringify(pacientes);
+//         // console.log("pacientes = JSON.stringify(pacientes); "+pacientes);
+//         // console.log(typeof pacientes);
+//     }else{
+//         console.log(xhr.status);
+//         console.log(xhr.responseText);
+//     }
+// });
+//
+// xhr.send();
