c9ea0e8cdd032d323795d1f5fba241ecad09ac46
Alexandre Ximenes <xyymenes@gmail.com>
2018-09-29 10:14:12 -0300

    init project

---
+++ Introducao ao JavaScript O.O/js/crud-paciente.js
@@ -0,0 +1,81 @@
+var tabela = document.querySelector("#tabela-pacientes");
+
+/**
+ * Remove uma pessoa da tabela
+ */
+tabela.addEventListener('dblclick', (e) => {
+    let TR = e.target.parentNode;
+    setTimeout(() => {
+        if(confirm('Deseja realmente deletar este paciente ? ')){
+            TR.classList.add('fadeout');
+            TR.remove();
+        }
+    }, 500);
+});
+
+/**
+ * Copia a linha (pessoa) clicada para a tabela
+ */
+tabela.addEventListener('click', (e) => {
+    e.preventDefault();
+    let tdNome = e.path[0].className;
+    console.log(tdNome);
+    let TR = e.target.parentNode;
+    if(tdNome === 'info-nome'){
+
+        var pessoa = capturaPessoa(TR);
+
+        var paciente = getPaciente(pessoa.nome, pessoa.peso, pessoa.altura, pessoa.gordura);
+
+        adicionarPacienteNaTabela(paciente);
+    }
+
+    form.reset();
+    form.nome.focus();
+
+});
+
+let body = document.querySelector("body");
+body.addEventListener('click', (e) => {
+    //console.clear();
+    // console.log(e.screenX);
+    // console.log(e.screenY);
+    console.log(e.target.parentNode);
+    insereDadosNoFormulario(e.target.parentNode);
+});
+
+tabela.addEventListener('mouseover', (e) => {
+    let TR = e.target.parentNode;
+    // console.log(TR);
+    insereDadosNoFormulario(TR);
+});
+
+function insereDadosNoFormulario(TR){
+    let form = document.querySelector("#form-add");
+
+    var pessoa = capturaPessoa(TR);
+
+    form.nome.value = pessoa.nome;
+    form.peso.value = pessoa.peso;
+    form.altura.value = pessoa.altura;
+    form.gordura.value = pessoa.gordura;
+
+}
+function capturaPessoa(TR) {
+    let nome = TR.querySelector('.info-nome').textContent;
+    let peso = TR.querySelector('.info-peso').textContent;
+    let altura = TR.querySelector('.info-altura').textContent;
+    let gordura = TR.querySelector('.info-gordura').textContent;
+
+    return getPaciente(nome, peso, altura, gordura);
+}
+
+/*
+var pacientes = document.querySelectorAll(".paciente");
+
+document.querySelector('#tabela')
+        .forEach((paciente) => {
+            paciente.addEventListener('dblclick', (e) => {
+
+    });
+});*/
---
+++ Introducao ao JavaScript O.O/js/form.js
@@ -0,0 +1,125 @@
+let form = document.querySelector("#form-add");
+let ul = document.querySelector(".ul");
+
+function validaPaciente(pessoa) {
+    let erros = [];
+    pessoa.nome == "" || pessoa.length <= 2 ? erros.push('Nome invalido!') : '';
+    pessoa.peso == "" || (pessoa.peso <= 0 || pessoa.peso > 1000) ? erros.push('Peso invalido!') : '';
+    pessoa.altura == "" || (pessoa.altura <= 0 || pessoa.altura >= 4.0) ? erros.push('Altura invalida!') : '';
+    pessoa.gordura == "" || pessoa.gordura < 0 || pessoa.gordura > pessoa.peso ? erros.push('Gordura invalida!') : '';
+    // console.log(pessoa);
+    return erros;
+}
+
+function getPaciente(nome, peso, altura, gordura){
+
+    let pessoa = {
+        nome : nome,
+        peso : peso,
+        altura : altura,
+        gordura : gordura,
+        imc : (peso / (altura * altura)).toFixed(2) //this.peso / (this.altura * this.altura)).toFixed(2)
+    };
+    return pessoa;
+}
+
+document.querySelector('#adicionar-paciente')
+    .addEventListener('click', (e) => {
+        e.preventDefault();
+        /**
+         *
+         * @type {{nome: *, altura: *, peso: *, gordura: *}}
+         */
+        var paciente = getPaciente(form.nome.value, form.peso.value, form.altura.value, form.gordura.value);
+        var gerou = adicionarPacienteNaTabela(paciente);
+        if(gerou){
+            form.reset();
+            form.nome.focus();
+        }
+    });
+
+function adicionarPacienteNaTabela(paciente){
+    //let pessoa = getPaciente(paciente.nome, paciente.peso, paciente.altura, paciente.gordura);
+    if(paciente != null){
+        let erros = validaPaciente(paciente);
+        if(erros.length > 0) {
+            console.log(erros);
+            cria_li(erros);
+            return false;
+        }else{
+            criaTr(paciente);
+            ul.innerHTML = '';
+            ul.classList.remove('erros');
+
+            //using template string
+            console.log(`${paciente.nome} tem ${paciente.altura}mt de altura e ${paciente.peso}Kg, com gordura de ${paciente.gordura}, totalizando IMC de : ${paciente.imc}`);
+            return true;
+        }
+    }
+
+}
+
+function cria_li(erros){
+    ul.classList.remove('fadeout');
+    ul.innerHTML = '';
+    ul.classList.add('erros');
+    erros.forEach(erro => {
+        let li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+    setTimeout(() => {
+        ul.classList.add('fadeout');
+    }, 4000);
+}
+function criaTr(pessoa){
+    /**
+     * Cria um TR
+     * @type {HTMLTableRowElement}
+     */
+    let pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+
+    /**
+     * Cria varias colunas TD para colocar dentro da TR
+     * @see pacienteTr
+     * @type {HTMLTableDataCellElement}
+     */
+
+    function montarTD(clazz, conteudo) {
+        let td = document.createElement("td");
+        td.classList.add(clazz);
+        td.textContent = conteudo;
+        return td;
+    }
+
+    let nomeTd = montarTD('info-nome', pessoa.nome);//document.createElement("td");
+    let pesoTd = montarTD('info-peso', pessoa.peso);//document.createElement("td");
+    let alturaTd = montarTD('info-altura', pessoa.altura);//document.createElement("td");
+    let gorduraTd = montarTD('info-gordura', pessoa.gordura);//document.createElement("td");
+    let imcTd = montarTD('info-imc', pessoa.imc);//document.createElement("td");
+
+
+    /**
+     * Coloca as TDs criadas dentro da TR criada
+     * @see pacienteTr
+     * @see nomeTd, pesoTd, alturaTd, gorduraTd, imcTd
+     */
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    /**
+     * Atribui a tabela da pagina index.html, a TR criada
+     * @see <table>tabela-pacientes</table>
+     * @type {HTMLTableRowElement}
+     */
+    let table = document
+        .querySelector("#tabela-pacientes")
+        .appendChild(pacienteTr);
+
+    return table;
+}
+
