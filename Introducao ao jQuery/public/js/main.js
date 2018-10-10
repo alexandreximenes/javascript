c1246cf1056c95a412351643387751022b24fd97
Alexandre Ximenes <xyymenes@gmail.com>
2018-10-09 23:34:50 -0300

    criando elementos com jQuery

--- Introducao ao jQuery/public/js/main.js
+++ Introducao ao jQuery/public/js/main.js
@@ -1,5 +1,80 @@
+let tempoInicial = $('#tempo-digitacao').text();
 
+$( () => {
+    inicializaContadores();
+    cronometro();
+    reiniciarJogo();
+    inicializaCorretorOrtografico();
+    $('#botao-reiniciar').click( reiniciarJogo );
 
+});
+
+let frase = $('.frase').text();
+let palavras = frase.split(' ');
+let numeroPalavras = palavras.length;
+let spanTamanhoFrase = $('#tamanho-frase');
+let spanContadorCaracteres = $('#contador-caracteres');
+let spanContadorPalavras = $('#contador-palavras');
+let spanTempoDigitacao = $('#tempo-digitacao');
+let tempoDigitacao = spanTempoDigitacao.text();
+spanTamanhoFrase.text(numeroPalavras);
+let campoDigitacao = $('.campo-digitacao');
+
+function inicializaCorretorOrtografico(){
+    campoDigitacao.on('input', () => {
+        let digitado = campoDigitacao.val();
+        var comparavel = frase.substr(0, digitado.length);
+        // console.log(digitado);
+        // console.warn(comparavel);
+        if(digitado == comparavel){
+            campoDigitacao.addClass("borda-verde");
+            campoDigitacao.removeClass("borda-vermelha");
+        }else{
+            campoDigitacao.removeClass("borda-verde");
+            campoDigitacao.addClass("borda-vermelha");
+        }
+    });
+}
+
+function inicializaContadores(){
+    campoDigitacao.on('input', () => {
+        let conteudo = campoDigitacao.val();
+        let qtdPalavras = conteudo.split(" ");
+        let qtdCaracteres = conteudo.length - 1;
+        spanContadorPalavras.text(qtdPalavras.length -1);
+        spanContadorCaracteres.text(qtdCaracteres);
+    });
+}
+
+function cronometro(){
+let tempoRestante = spanTempoDigitacao.text();
+    campoDigitacao.one('focus', () => {
+        campoDigitacao.removeClass("borda-verde");
+        campoDigitacao.removeClass("borda-vermelha");
+        let tempoId = setInterval(() => {
+            spanTempoDigitacao.text(--tempoRestante);
+            if(tempoRestante == 0){
+                clearInterval(tempoId);
+                finalizaJogo();
+            }
+        }, 1000)
+    });
+}
+
+function finalizaJogo(){
+    campoDigitacao.attr('disabled', true);
+    campoDigitacao.addClass('campo-desativado');
+    inserePlacar();
+}
+function reiniciarJogo(){
+        campoDigitacao.attr('disabled', false);
+        campoDigitacao.val("");
+        campoDigitacao.removeClass("campo-desativado");
+        spanTempoDigitacao.text(tempoInicial);
+        spanContadorCaracteres.text("0");
+        spanContadorPalavras.text("0");
+        cronometro();
+}
 // var tempoInicial = $("#tempo-digitacao").text();
 // var campo = $(".campo-digitacao");
 //
--- Introducao ao jQuery/public/js/placar.js
+++ Introducao ao jQuery/public/js/placar.js
@@ -1,4 +1,41 @@
+$( () => {
 
+});
+var corpoTabela = $('.placar').find('tbody');
+function inserePlacar(){
+    let numPalavras = $('#contador-palavras').text();
+    let usuario = prompt("Qual seu nome: ");
+    let linha = `<tr>
+                    <td>${usuario}</td>
+                    <td>${numPalavras}</td>
+                    <td><a class="botao-remover" href="#"><i class="botao-remover small material-icons">delete</i></a></td>
+                </tr>`;
+
+    $('.placar').removeClass('placar');
+    corpoTabela.prepend(linha);
+}
+$('.botao-remover');
+
+function novaLinha(usuario, numPalavras){
+    let tr = $("<tr>");
+    let tdUsuario = $("<td>");
+    let tdNumPalavras = $("<td>");
+    let tdRemover = $("<td>");
+    tdUsuario.text(usuario);
+    tdNumPalavras.text(numPalavras);
+    let link = $("<a>").addClass('botao-remover').attr("href", "#");
+
+    let icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+    link.append(icone);
+    tdRemover.append(link);
+
+
+    tr.append(tdUsuario);
+    tr.append(tdNumPalavras);
+    tr.append(tdRemover);
+
+    return tr;
+}
 
 // var tempoScroll = 1000;
 //
